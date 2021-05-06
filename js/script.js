793f35d2864477d69a240f3e024c118f21bfb371
aude <aude.lalanne4@gmail.com>
2021-05-06 14:35:13 +0200

    Javascript => optimisation

--- js/script.js
+++ js/script.js
@@ -12,26 +12,18 @@ function Player (firstname, hand, global) {
 // Instances Players dans mon tableau 
 let players =  [ 
     new Player ('Aude', 1, 0 ), 
-    new Player ('Linh', 1, 0 )
+    new Player ('Linh', 0, 0 )
 ]
 
 // Div to display scores per players
-let divScoreCurrentPlayer1 = document.getElementById('divScoreCurrentPlayer1')
-let divScoreCurrentPlayer2 = document.getElementById('divScoreCurrentPlayer2')
+let divScoreCurrent = document.getElementById('divScoreCurrent')
 let player1DivGlobal = document.getElementById('player1Global')
 let player2DivGlobal = document.getElementById('player2Global')
-
 // Start with player 1
-var divScoreCurrent = divScoreCurrentPlayer1 // div par defaut du joueur en cours, pour le current score
-var activePlayerGlobal = players[0].global //score global du joueur en cours
-var activePlayer = players[0].firstname //nom du joueur en cours
 var playerDivGlobal = player1DivGlobal // div par défaut du joueur en cours pour le global score
 
 // Start Game
 function partie () {
-
-    // Round tour à tour
-    function play () {
 
         // Dice init
         var dice = document.getElementById('dice') 
@@ -42,64 +34,36 @@ function partie () {
             min = Math.ceil(1);
             max = Math.floor(7);
             currentDice = Math.floor(Math.random() * (max - min)) + min;
+            console.log(currentDice)
             divScoreCurrent.innerHTML = currentDice 
         }
+        dice.addEventListener('click', getRandomInt)
 
         // Recover dice value
-            function recoverValue () {
+        function recoverValue () {
 
-                // Player1 plays
-                if (players[0].hand === 1) { 
-                    // Add score to Player1 Global 
-                    players[0].global = players[0].global + currentDice
-                    playerDivGlobal.innerHTML = players[0].global // display in global div 
-                    divScoreCurrent.innerHTML = 0 // current score at 0 
-                    // changement de main       
-                    players[0].hand = 0
-                    players[1].hand = 1
-                    activePlayerGlobal = players[1].global
-                    activePlayer = players[1].firstname
-                    divScoreCurrent = divScoreCurrentPlayer2
-                    playerDivGlobal = player2DivGlobal
-                } 
+            // je défini l'index de mon player en cours
+            playerGo = players.map(function(e) { return e.hand; }).indexOf(1);
+            playerWait = players.map(function(e) { return e.hand; }).indexOf(0);
 
-                // Player2 plays
-                else {
-                    // Add score to Player1 Global 
-                    players[1].global = players[1].global + currentDice
-                    playerDivGlobal.innerHTML = players[1].global // display in global div 
-                    divScoreCurrent.innerHTML = 0 // current score at 0                     
-                    // changement de main
-                    players[0].hand = 1
-                    players[1].hand = 0
-                    activePlayerGlobal = players[0].global
-                    activePlayer = players[0].firstname
-                    divScoreCurrent = divScoreCurrentPlayer1
-                    playerDivGlobal = player1DivGlobal
-                }  
-              
-                // Done
-                if  (players[0].global > toReach) {
-                    alert('Partie terminée !')
-                } 
-            }
+            console.log(players[playerGo].firstname)
+            console.log(divScoreCurrent)
 
-            var recover = document.getElementById('recover')
-            recover.addEventListener('click', recoverValue)
-
-        // gaming : tour à tour
-        function tour() {
-            if (activePlayerGlobal <= toReach) { 
-                if (players[0].hand == 1) { 
-                    dice.addEventListener('click', getRandomInt)
-                } 
-                if (players[1].hand == 1) {
-                    dice.addEventListener('click', getRandomInt)
-                }   
+            // je defini les div en cours
+            players[0].hand === 1 ? playerDivGlobal = player1DivGlobal : playerDivGlobal = player2DivGlobal 
+            // ajout du score
+            players[playerGo].global = players[playerGo].global + currentDice
+            playerDivGlobal.innerHTML = players[playerGo].global // display in global div 
+            divScoreCurrent.innerHTML = 0 // current score at 0
+            // changement de main 
+            players[playerGo].hand = 0
+            players[playerWait].hand = 1
+            // Done
+            if  (players[0].global > toReach) {
+                alert('Partie terminée !')
             } 
         }
-        tour()
-    }
-    play()
+        var recover = document.getElementById('recover')
+        recover.addEventListener('click', recoverValue)
 }
 partie()
