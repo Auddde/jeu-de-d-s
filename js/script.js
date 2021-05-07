e195f8d57c0bab3ea0ad5fd402d02e160c8cf6c0
aude <aude.lalanne4@gmail.com>
2021-05-07 14:13:36 +0200

    Javascript, finalisation d'une partie

--- js/script.js
+++ js/script.js
@@ -18,19 +18,40 @@ let players =  [
 let divScoreCurrent = document.getElementById('divScoreCurrent')
 let player1DivGlobal = document.getElementById('player1Global')
 let player2DivGlobal = document.getElementById('player2Global')
+let prenomDivCurrent = document.getElementById('prenomCurrent')
+let cursorDiv1 = document.getElementById('triangle1')
+let cursorDiv2 = document.getElementById('triangle2')
+let player1Name = document.getElementById('player1')
+let player2Name = document.getElementById('player2')
+
+//Affichage des prénoms
+player1Name.innerHTML = players[0].firstname
+player2Name.innerHTML = players[1].firstname
+
 // Start with player 1
 var playerDivGlobal = player1DivGlobal // div par défaut du joueur en cours pour le global score
+prenomCurrent = players[0].firstname
+prenomDivCurrent.innerHTML = prenomCurrent
+cursorDiv2.style.display = 'none';
+// Button recover :  not displayed before playing
+let divInteractions = document.getElementById('divInteractions')
+divInteractions.style.display = "none"; 
+let or = document.getElementById('or')
 
 // Start Game
 function partie () {
 
         // Dice init
         var dice = document.getElementById('dice') 
-        var currentDice // Initialize current score dice for the current player 
+        var currentDice = 0 // Initialize current score dice for the current player 
         var diceImage = document.getElementById('diceImage')  
+        diceImage.innerHTML = `<img src="images/dice-0.png" class="imageDice">`
         
         // Roll the dice
         function getRandomInt(min, max) { // Roll the dice, value at random
+
+            divInteractions.style.display = "block"; 
+
             currentDice = 0;
             min = Math.ceil(1);
             max = Math.floor(7);
@@ -41,16 +62,23 @@ function partie () {
                 diceImage.innerHTML = content
             }
             imageDisplay(currentDice)
-            divScoreCurrent.innerHTML = currentDice
+
+            currentDice === 1 ? wordingRecover = `Libère tes maïs !` : wordingRecover = `Récupère ces <b>${currentDice} </b>maïs`
+            currentDice === 1 ? wordingRecover = `Libère tes maïs !` : wordingRecover = `Récupère ces <b>${currentDice} </b>maïs`
+            if (currentDice === 1) { or.style.display = "none"; } else {or.style.display = "block";}
+            divScoreCurrent.innerHTML = wordingRecover
+
             // 1 ! 
             if (currentDice === 1) {
-                recoverValue(1)
+                diceImage.innerHTML = `<img src="images/dice-1.png" class="imageDice">`
+                dice.removeEventListener('click', getRandomInt); //éviter la triche en recliquant
             }
         }
+
         dice.addEventListener('click', getRandomInt)
 
         // Recover dice value
-        function recoverValue (i) {
+        function recoverValue () {
 
                 // je défini l'index de mon player en cours
                 playerGo = players.map(function(e) { return e.hand; }).indexOf(1);
@@ -58,21 +86,38 @@ function partie () {
                 // je defini les div en cours
                 players[0].hand === 1 ? playerDivGlobal = player1DivGlobal : playerDivGlobal = player2DivGlobal 
                 // ajout du score
-                i === 1 ? players[playerGo].global = 0 : players[playerGo].global = players[playerGo].global + currentDice 
+                if (currentDice === 1) {
+                    players[playerGo].global = 0 
+                    dice.addEventListener('click', getRandomInt)
+                } else {
+                    players[playerGo].global = players[playerGo].global + currentDice
+                }
                 playerDivGlobal.innerHTML = players[playerGo].global // display in global div 
-                divScoreCurrent.innerHTML = 0 // current score at 0 
+
+                //A DEGAGER =>divScoreCurrent.innerHTML = 0 // current score at 0  
+                divInteractions.style.display = "none"; 
+
                 // changement de main 
+                if (playerGo == 0) {
+                    cursorDiv1.style.display = 'none'; 
+                    cursorDiv2.style.display = 'block';
+                } else {
+                    cursorDiv1.style.display = 'block'; 
+                    cursorDiv2.style.display = 'none';                    
+                }
                 players[playerGo].hand = 0
                 players[playerWait].hand = 1
-            
+                prenomDivCurrent.innerHTML = players[playerWait].firstname
+                diceImage.innerHTML = `<img src="images/dice-0.png" class="imageDice">`
+                currentDice = 0 // reinitialize le dé
+       
             // Done
-            if  (players[0].global > toReach) {
+            if  ( (players[0].global > toReach) || (players[1].global > toReach)) {
                 alert('Partie terminée !')
             } 
         }
 
         var recover = document.getElementById('recover')
         recover.addEventListener('click', recoverValue)
-
 }
 partie()
